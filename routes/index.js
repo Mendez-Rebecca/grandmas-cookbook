c50fd7acf578b96042f603cbc5b55d9cf9738e44
Rebecca <mendez_elizabeth88@hotmail.com>
2023-08-24 18:08:48 -0700

    add mongoose, OAuth and all related code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
+});
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    prompt: 'select_account'
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: 'index',
+    failureRedirect: 'index'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/movies');
+  });
 });
 
 module.exports = router;
