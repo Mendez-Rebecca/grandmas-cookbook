af269727d794b6535bdeb6a11895f6417ac05cfc
Rebecca <mendez_elizabeth88@hotmail.com>
2023-08-28 15:43:01 -0700

    blue theme logic and styling complete

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -13,6 +13,8 @@ function openNav() {
     document.getElementById('hamburger').style.visibility = 'hidden';
 }
 
+applySavedTheme();
+
 let isExpanded = false;
 
 function themeDropdown() {
@@ -22,7 +24,6 @@ function themeDropdown() {
     if (!isExpanded) {
         themeButton.innerHTML = "THEMES v";
 
-        // Create and append the theme links
         const blueThemeLink = document.createElement('a');
         blueThemeLink.id = "blue-theme";
         blueThemeLink.className = "blue-theme";
@@ -35,26 +36,55 @@ function themeDropdown() {
         greenThemeLink.textContent = "GREEN";
         greenThemeLink.onclick = greenTheme;
 
+        const orangeThemeLink = document.createElement('a');
+        orangeThemeLink.id = "orange-theme";
+        orangeThemeLink.className = "orange-theme";
+        orangeThemeLink.textContent = "ORANGE";
+        orangeThemeLink.onclick = orangeTheme;
+
         themeLinksContainer.appendChild(blueThemeLink);
         themeLinksContainer.appendChild(document.createElement('br'));
         themeLinksContainer.appendChild(document.createElement('br'));
         themeLinksContainer.appendChild(greenThemeLink);
         themeLinksContainer.appendChild(document.createElement('br'));
         themeLinksContainer.appendChild(document.createElement('br'));
+        themeLinksContainer.appendChild(orangeThemeLink);
+        themeLinksContainer.appendChild(document.createElement('br'));
+        themeLinksContainer.appendChild(document.createElement('br'));
     } else {
         themeButton.innerHTML = "THEMES >";
-
-        // Clear the theme links
         themeLinksContainer.innerHTML = '';
     }
-
     isExpanded = !isExpanded;
 }
 
 function blueTheme() {
-
+    localStorage.setItem('selectedTheme', 'blue');
+    applySavedTheme();
 }
 
 function greenTheme() {
+    localStorage.setItem('selectedTheme', 'green');
+    applySavedTheme();
+}
 
+function orangeTheme() {
+    localStorage.setItem('selectedTheme', 'orange');
+    applySavedTheme();
+}
+
+function applySavedTheme() {
+    const savedTheme = localStorage.getItem('selectedTheme');
+    const bodyElement = document.body;
+
+    if (savedTheme === 'blue') {
+        bodyElement.classList.remove('orange', 'green');
+        bodyElement.classList.add('blue');
+    } else if (savedTheme === 'green') {
+        bodyElement.classList.remove('orange', 'blue');
+        bodyElement.classList.add('green');
+    } else if (savedTheme === 'orange') {
+        bodyElement.classList.remove('blue', 'green');
+        bodyElement.classList.add('orange');
+    }
 }
